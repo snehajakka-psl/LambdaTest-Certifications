e855ce1a703bf70a61d2925db7d0b5a026f2cc7f
Sneha Jakka <sneha_jakka@persistent.com>
2024-03-13 14:48:14 +0000

    Scenario1

---
+++ tests/scenario1.spec.js
@@ -0,0 +1,16 @@
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+    let message = 'Welcome to Lambda test'
+    let url='https://www.lambdatest.com/selenium-playground/simple-form-demo'
+    await page.goto('https://www.lambdatest.com/selenium-playground/');
+    await page.goto(url);
+    expect(url).ttoContainText('ssimple-form-demo');
+    await page.getByPlaceholder('Please enter your Message').click();
+    await page.getByPlaceholder('Please enter your Message').fill.message;
+    await page.getByRole('button', { name: 'Get Checked Value' }).click();
+    await page.getByText('Welcome to Lambda test').isVisible();
+});
+
